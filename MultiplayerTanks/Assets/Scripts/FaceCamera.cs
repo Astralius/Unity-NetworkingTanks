9e8848731c085116913ba45e1462dd043fc3bf33
Adrian Standowicz <adrian.standowicz@gmail.com>
2018-09-26 21:46:05 +0200

    Camera facing HUD
    Added script for world-space HUD to face selected camera. Added fancy font for the HUD.

---
+++ MultiplayerTanks/Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private new Transform transform;
+    private Transform cameraTransform;
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        transform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        transform.LookAt(worldPosition: transform.position + cameraTransform.rotation * Vector3.forward,
+                         worldUp: cameraTransform.rotation * Vector3.up);
+    }
+}
