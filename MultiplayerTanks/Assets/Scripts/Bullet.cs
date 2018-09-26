7755f1d721257dddc1198cb81aeb1d306369a0fa
Adrian Standowicz <adrian.standowicz@gmail.com>
2018-09-26 22:05:52 +0200

    Tank Shooting
    Added shooting mechanism for tanks, incl. bouncing bullet prefab and related assets.

---
+++ MultiplayerTanks/Assets/Scripts/Bullet.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : NetworkBehaviour
+{
+    public int Speed = 100;
+    public float Lifetime = 5f;
+    public int Bounces = 2;
+    public int Damage = 1;
+    public GameObject ExplosionEffect;
+    public List<string> BounceTags;
+    public List<string> CollisionTags;
+
+    private List<ParticleSystem> allParticles;
+    private new Rigidbody rigidbody;
+    private new Collider collider;
+
+    private void Start()
+    {
+        allParticles = GetComponentsInChildren<ParticleSystem>().ToList();
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.forward * Speed;
+        collider = GetComponent<Collider>();
+        StartCoroutine(SelfdestructAfterDelay(Lifetime));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        CheckCollision(collision);
+        CheckBounce(collision);
+    }
+
+    private void CheckCollision(Collision collision)
+    {
+        if (CollisionTags.Contains(collision.gameObject.tag))
+        {          
+            var playerHealth = collision.gameObject.GetComponentInParent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.Damage(Damage);
+            }
+            SelfDestruct();
+        }
+    }
+
+    private void CheckBounce(Collision collision)
+    {
+        if (BounceTags.Contains(collision.gameObject.tag))
+        {
+            if (Bounces <= 0)
+            {
+                SelfDestruct();
+            }
+            else
+            {
+                Bounces--;
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (rigidbody.velocity != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(rigidbody.velocity);
+        }
+    }
+
+    private IEnumerator SelfdestructAfterDelay(float lifetime)
+    {
+        yield return new WaitForSeconds(lifetime);
+        SelfDestruct();
+    }
+
+    private void SelfDestruct()
+    {
+        collider.enabled = false;
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.Sleep();
+
+        foreach (ParticleSystem ps in allParticles)
+        {
+            ps.Stop();
+        }
+
+        if (ExplosionEffect != null)
+        {
+            var explosion = Instantiate(ExplosionEffect, transform.position, Quaternion.identity);
+            GameObject.Destroy(explosion, 2f);
+        }
+
+        if (isServer)
+        {
+            Destroy(this.gameObject);
+        }       
+    }
+}
--- MultiplayerTanks/Assets/Scripts/PlayerShooter.cs
+++ MultiplayerTanks/Assets/Scripts/PlayerShooter.cs
@@ -1,18 +1,52 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
 
 /// <summary>
 /// Class for controlling how player's tank shoots projectiles.
 /// </summary>
-public class PlayerShooter : MonoBehaviour
+public class PlayerShooter : NetworkBehaviour
 {
+    public Rigidbody BulletPrefab;
+    public Transform BulletSpawn;
+    public int ShotsPerRound = 2;
+    public float RealoadTime = 1f;
+
+    private int shotsLeft;
+    private bool isReloading;
+
 
     private void Start()
     {
-
+        shotsLeft = ShotsPerRound;
+        isReloading = false;
     }
 
-    private void Update()
+    public void Shoot()
     {
+        if (!isReloading && BulletPrefab != null)
+        {
+            CmdShoot();
+            shotsLeft--;
+            if (shotsLeft <= 0)
+            {
+                StartCoroutine(Reload());
+            }
+        }
+    }
 
+    [Command]
+    private void CmdShoot()
+    {
+        var bullet = Instantiate(BulletPrefab, BulletSpawn.position, BulletSpawn.rotation);
+        NetworkServer.Spawn(bullet.gameObject);
+    }
+
+    private IEnumerator Reload()
+    {
+        shotsLeft = ShotsPerRound;
+        isReloading = true;
+        yield return new WaitForSeconds(RealoadTime);
+        isReloading = false;
     }
 }
