567b018511672a68e8389c3d2dbc2be9f801be46
Adrian Standowicz <adrian.standowicz@gmail.com>
2019-01-14 20:50:03 +0100

    Added spawn point occupants logic
    Spawn points will now keep track of how many tanks are within their trigger collider, to prevent spawning tanks on top of each other.

---
+++ MultiplayerTanks/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [ReadOnly]
+    public bool IsOccupied;
+
+    private readonly IList<PlayerController> occupants = new List<PlayerController>();
+
+    public void OccupantLeft(PlayerController occupant)
+    {
+        occupants.Remove(occupant);
+        IsOccupied = occupants.Count > 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var controller = other.GetComponent<PlayerController>();
+        if (controller != null)
+        {
+            occupants.Add(controller);
+            IsOccupied = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var controller = other.GetComponent<PlayerController>();
+        if (controller != null)
+        {
+            occupants.Remove(controller);
+            IsOccupied = occupants.Count > 0;
+        }
+    }
+}
